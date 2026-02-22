fc42b906a2326c88b3e8f422309d85bb45b57d69
kishanhitk <kishansharma1231@gmail.com>
2026-02-22 22:18:43 +0530

    Make preflight script bash-3 compatible

--- scripts/e2e_preflight.sh
+++ scripts/e2e_preflight.sh
@@ -18,10 +18,14 @@ require_file "${ROOT_DIR}/VibeStoke/Frameworks/libonnxruntime.dylib"
 echo "[2/3] Typechecking app sources with C API bridge"
 cd "${ROOT_DIR}"
 if command -v rg >/dev/null 2>&1; then
-  mapfile -t SWIFT_FILES < <(rg --files VibeStoke -g '*.swift')
+  SWIFT_FILE_CMD=(rg --files VibeStoke -g '*.swift')
 else
-  mapfile -t SWIFT_FILES < <(find VibeStoke -type f -name '*.swift' | sort)
+  SWIFT_FILE_CMD=(find VibeStoke -type f -name '*.swift')
 fi
+SWIFT_FILES=()
+while IFS= read -r file; do
+  SWIFT_FILES+=("${file}")
+done < <("${SWIFT_FILE_CMD[@]}" | sort)
 swiftc -import-objc-header VibeStoke/VibeStoke-Bridging-Header.h -typecheck "${SWIFT_FILES[@]}"
 
 echo "[3/3] Generating project spec"
